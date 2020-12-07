a32488b87869a01619106ef034f2a2a1cea8753f
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2020-12-07 17:56:30 -0500

    Updated skills + projects section + button transitions

--- ipe.js
+++ ipe.js
@@ -1,1 +1,12 @@
+/*const projects = [
+    {
+        'name': 'Bad Ice-Cream',
+        'date': new Date(2019, 0, 8, 0, 0, 0, 0),
+        'tech': ['Java'],
+        'images': [],
+        'description': ''
+    }
+
+];*/
+
 // for animations!
